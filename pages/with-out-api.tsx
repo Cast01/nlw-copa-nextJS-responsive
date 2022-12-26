ed3efce6780c332bf96b86a778891d3ed7e60926
Cast01 <gabriel_floriano_castilho@outlook.com>
2022-12-26 17:11:27 -0300

    UPDATE(Refatorando, Limpando)

--- pages/with-out-api.tsx
+++ pages/with-out-api.tsx
@@ -5,8 +5,8 @@ export default function WithOutAPI() {
         <div className="text-white min-h-screen text-3xl flex flex-col items-center justify-center text-center p-8 gap-6">
             <Header />
             <h1>Faça um clone do Front-End e da API para conseguir testar o projeto.</h1>
-            <a className="text-blue-500 underline mt-6" href="https://github.com/Cast01/nlw-copa-nextJS-responsive" target={"_blank"}>FRONT-END</a>
-            <a className="text-blue-500 underline" href="https://github.com/Cast01/nlw-copa-API-fastify" target={"_blank"}>API</a>
+            <a className="text-blue-500 underline mt-6" href="https://github.com/Cast01/nlw-copa-nextJS-responsive" target={"_blank"} rel="noreferrer">FRONT-END</a>
+            <a className="text-blue-500 underline" href="https://github.com/Cast01/nlw-copa-API-fastify" target={"_blank"} rel="noreferrer">API</a>
         </div>
     );
 }
